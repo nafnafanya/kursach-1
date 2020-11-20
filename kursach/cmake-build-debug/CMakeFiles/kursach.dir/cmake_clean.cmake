e2b639cf06a903a7d66a3dbcb87201921765def3
craftmaster1231 <32911478+craftmaster1231@users.noreply.github.com>
2020-11-20 21:34:46 +0300

    Add files via upload

---
+++ kursach/cmake-build-debug/CMakeFiles/kursach.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kursach.dir/main.cpp.obj"
+  "kursach.exe"
+  "kursach.exe.manifest"
+  "kursach.pdb"
+  "libkursach.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kursach.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
