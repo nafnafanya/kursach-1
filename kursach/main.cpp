d3fd0be586fdf1a708dc3fe0ab8c2e6ebcfb7520
craftmaster1231 <32911478+craftmaster1231@users.noreply.github.com>
2020-11-23 00:56:22 +0300

    Update main.cpp

--- kursach/main.cpp
+++ kursach/main.cpp
@@ -17,14 +17,14 @@ std::string readFilePlayerLevel(){
     const std::string file_name = "player_level.txt";
     std::ifstream  fs;
     fs.open(file_name);
-    fs.eof();
     fs >> level;
     fs.close();
     return level;
-
 }
 
 
 int main() {
+    savePlayerLevelInFile(345345);
+    std::cout<<readFilePlayerLevel();
     return 0;
 }
