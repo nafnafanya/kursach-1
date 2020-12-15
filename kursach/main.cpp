b5fa75a216f7e82b3737c317c917a88c9e64f472
craftmaster1231 <32911478+craftmaster1231@users.noreply.github.com>
2020-12-15 23:20:07 +0300

    functions in class
    Added some functions in class.Also file functions moved to class. Fixed file reading function. return type is string now.

--- kursach/main.cpp
+++ kursach/main.cpp
@@ -1,13 +1,15 @@
-
 #include <iostream>
 #include <Windows.h>
 #include <fstream>
 #include <string>
-  
-  
-class hero 
+
+
+class hero
 {
 public:
+    hero(){
+        set_level(readFilePlayerLevel());
+    }
     void plusGood()
     {
         good += 1;
@@ -27,36 +29,55 @@ public:
     void plusLevel()
     {
         level += 1;
+        savePlayerLevelInFile(level);
+
+    }
+    void set_level(int level){
+        this->level=level;
     }
     int get_level()
     {
         return level;
     }
-private: 
+private:
+    int ten_degree(int degree){
+        int value = 1;
+        for(int i = 0;i<degree;i++){
+            value*=10;
+        }
+        return value;
+    }
+
+    int string_to_int(std::string level_string){
+        int level_int = 0;
+        int length = level_string.length();
+        for (int i=length, degree=0;i>0;i--,degree++){
+            level_int+=(level_string[i-1]-48)*ten_degree(degree);
+        }
+        return level_int;
+    }
+    int readFilePlayerLevel() {
+        std::string level;
+        const std::string file_name = "player_level.txt";
+        std::ifstream  fs;
+        fs.open(file_name);
+        fs >> level;
+        fs.close();
+
+        return string_to_int(level);
+    }
+
+    void savePlayerLevelInFile(int level) {
+        const std::string file_name = "player_level.txt";
+        std::ofstream fs;
+        fs.open(file_name);
+        fs << level;
+        fs.close();
+    }
     int good = 0;
     int bad = 0;
-    int level =0;
-    
+    int level = 0;
 };
-
-void savePlayerLevelInFile(int level) {
-    const std::string file_name = "player_level.txt";
-    std::ofstream fs;
-    fs.open(file_name);
-    fs << level;
-    fs.close();
-}
-
-
-std::string readFilePlayerLevel() {
-    std::string level;
-    const std::string file_name = "player_level.txt";
-    std::ifstream  fs;
-    fs.open(file_name);
-    fs >> level;
-    fs.close();
-    return level;
-}
 
 void print(char *str) {
     setlocale(0, "Russian");
@@ -72,8 +93,9 @@ struct story_point {
 };
 
 int main() {
+hero Katya;
+Katya.plusLevel();
+std::cout<<Katya.get_level();
     return 0;
 
 }
-
-
